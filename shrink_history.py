70c8a659d9f034ab77643688a00f6a449648a563
wh201906 <wh201906@yandex.com>
2023-08-26 00:18:24 +0800

    Shrink: works

--- shrink_history.py
+++ shrink_history.py
@@ -1,11 +1,10 @@
-import git, os
+import git, sys
 
 source_repo_path = "./test"
 target_repo_path = "./output"
 
 
 def has_changes(diff_result: git.DiffIndex):
-    # cannot handle commit with no parents/multiple parents
     return len(diff_result) > 0
 
 
@@ -23,59 +22,58 @@ def has_only_additions(diff_result: git.DiffIndex):
     return True
 
 
-# os.makedirs(target_repo_path)
-# target_repo = git.Repo().init(target_repo_path)
-source_repo = git.Repo(source_repo_path)
-# source_repo.git.execute("git checkout --orphan shrinked")
+repo = git.Repo(source_repo_path)
 
 commit_list = []
-new_commit_idx = 0
-new_commit_list = []
 
+last_commit_type = "add"
 is_addition_last = True
-for commit in source_repo.iter_commits(reverse=True):
+for commit in repo.iter_commits(reverse=True):
+    # new_commit: the previous commit should be picked
     new_commit = False
 
     # cannot handle commit with no parents/multiple parents
     parents_num = len(commit.parents)
     if parents_num > 1:
         print(f"Error: parents num is {parents_num} rather than 1")
-        continue
-    elif parents_num == 0:
-        is_addition_last = True
-    else:
+        sys.exit(0)
+    elif parents_num == 0:  # the first commit
+        last_commit_type = "add"  # OK if the first commit is empty
+    else:  # parents_num == 1
         diff_result = commit.diff(commit.parents[0], create_patch=True, R=True)
-        if not has_changes(diff_result):
+        additions_only = has_only_additions(diff_result)
+        changed = has_changes(diff_result)
+        if last_commit_type == "empty":  # shrink empty commits
             new_commit = False
-            # if it's the last commit?
-        elif has_only_additions(diff_result):
-            new_commit = not is_addition_last
-            is_addition_last = True
-        else:
+        elif last_commit_type == "add":
+            # shrink continuous addition-only commits
+            new_commit = False if (not changed) or additions_only else True
+        elif last_commit_type == "del":  # always pick commits with deletions
             new_commit = True
-            is_addition_last = False
-        if new_commit:
-            new_commit_list.append(new_commit_idx - 1)
-        print(
-            commit.hexsha,
-            has_changes(diff_result),
-            has_only_additions(diff_result),
-            new_commit,
-        )
+
+        # update last state
+        if not changed:
+            last_commit_type = "empty"
+        elif has_only_additions(diff_result):
+            last_commit_type = "add"
+        else:
+            last_commit_type = "del"
+
     commit_list.append((commit.hexsha, new_commit))
-    new_commit_idx += 1
 
 print(commit_list)
-print(new_commit_list)
+commit_num = len(commit_list)
+if commit_num < 2:
+    print(f"Error: this repo has only {commit_num} commit(s)")
+    sys.exit(0)
+commit_list.append(("end", True))  # pick the last commit
 
-commit_list[0]=(commit_list[0][0], True)
 last_item = commit_list[0]
-source_repo.git.checkout("-b", "shrinked", commit_list[0][0])
+repo.git.checkout("-b", "shrinked", commit_list[0][0])
 for commit_item in commit_list[1:]:
-    print(commit_item, last_item)
-    if commit_item[1] == True or last_item[1] == True:
-        commit_for_info = source_repo.commit(last_item[0])
-        source_repo.index.commit(
+    if commit_item[1] == True:
+        commit_for_info = repo.commit(last_item[0])
+        repo.index.commit(
             "Test",
             author=commit_for_info.author,
             committer=commit_for_info.committer,
@@ -83,22 +81,7 @@ for commit_item in commit_list[1:]:
             commit_date=commit_for_info.committed_datetime,
         )
     last_item = commit_item
-    source_repo.git.execute(
-        f"git cherry-pick --allow-empty --no-commit {commit_item[0]}"
-    )
 
-
-# source_repo.git.execute("git checkout --orphan shrinked")
-# source_repo.git.execute("git reset")
-# source_repo.git.execute("git clean -f")
-# start = commit_list[0][0]
-# for commit_idx in new_commit_list:
-#     print(commit_idx, commit_list[commit_idx])
-#     source_repo.git.execute(f"git cherry-pick {start}..{commit_list[commit_idx][0]} --allow-empty")
-#     start = commit_list[commit_idx][0]
-# # curr.parent=0: initial commit(treat as add)
-# # else
-# # curr = unchanged: merge
-# # curr = add, last != add, new
-# # curr = add, last = add, merge
-# # curr
+    if commit_item[0] == "end":
+        break
+    repo.git.execute(f"git cherry-pick --allow-empty --no-commit {commit_item[0]}")
